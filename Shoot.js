e80541fe3d9546fbe543eae50c8f75c5addfe725
Sylvain <aimozs@gmail.com>
2013-05-28 10:52:18 +1000

    first upload of what i got so far

---
+++ Shoot.js
@@ -0,0 +1,13 @@
+var bullitPrefab:GameObject;
+var spawnPoint : Transform;
+
+function Update () {
+
+	if(Input.GetButtonDown("Fire1"))
+	{
+		var bullit =  Instantiate(bullitPrefab, GameObject.Find("spawnPoint").transform.position, GameObject.Find("spawnPoint").transform.rotation);
+		bullit.rigidbody.velocity = spawnPoint.TransformDirection(Vector3(0,-100,0));
+	}
+	
+
+}
